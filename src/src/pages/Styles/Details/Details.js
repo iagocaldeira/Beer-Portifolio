808ec484be34400f3fe0f4a0b0ac56980a41ce1c
Iago Caldeira <iagocaldeiraa@gmail.com>
2018-02-08 13:07:43 -0200

    Modals details

--- src/src/pages/Styles/Details/Details.js
+++ src/src/pages/Styles/Details/Details.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 
 import './Details.css';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+    Modal, ModalHeader, ModalBody, ModalFooter, CardSubtitle, Button, Col, Badge, Row
+} from 'reactstrap';
 
 class Details extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            modal: false,
-            beer: {}
+            modal: false
         };
-
-        this.toggle = this.toggle.bind(this);
     }
 
-    toggle(fullBeer) {
+    toggle = () => {
         this.setState({
-            modal: !this.state.modal,
-            beer: fullBeer
+            modal: !this.state.modal
         });
     }
 
@@ -26,9 +24,70 @@ class Details extends Component {
         return (
             <div>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>{this.state.beer.nameDisplay}</ModalHeader>
+                    <ModalHeader toggle={this.toggle}>{this.props.style.nameDisplay}</ModalHeader>
                     <ModalBody>
-                        {this.state.beer.description}
+                        <CardSubtitle>
+                            <Row className="ibu-row justify-content-around" >
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Min IBU: <Badge color="light">{this.props.style.ibuMin}</Badge>
+                                    </Button>
+                                </Col>
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Max IBU: <Badge color="light">{this.props.style.ibuMax}</Badge>
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row className="ibu-row justify-content-around" >
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Min ABV: <Badge color="light">{this.props.style.abvMin}</Badge>
+                                    </Button>
+                                </Col>
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Max ABV: <Badge color="light">{this.props.style.abvMax}</Badge>
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row className="ibu-row justify-content-around" >
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Min SRM: <Badge color="light">{this.props.style.srmMin}</Badge>
+                                    </Button>
+                                </Col>
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Max SRM: <Badge color="light">{this.props.style.srmMax}</Badge>
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row className="ibu-row justify-content-around" >
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Min Original Gravity: <Badge color="light">{this.props.style.ogMin}</Badge>
+                                    </Button>
+                                </Col>
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Max Original Gravity: <Badge color="light">{this.props.style.ogMax}</Badge>
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row className="ibu-row justify-content-around" >
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Min Final Gravity: <Badge color="light">{this.props.style.fgMin}</Badge>
+                                    </Button>
+                                </Col>
+                                <Col xs="6" lg="auto">
+                                    <Button className="btn-block" color="dark" outline>
+                                        Max Final Gravity: <Badge color="light">{this.props.style.fgMax}</Badge>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </CardSubtitle>
                     </ModalBody>
                     <ModalFooter>
                         <Button color="secondary" onClick={this.toggle}>Go back</Button>
